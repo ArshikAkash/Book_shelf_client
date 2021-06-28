62c1239639522a0dccef89d96b21d0f86a909ab7
Arshik Hossen Akash <arshikakash@gmail.com>
2021-06-28 21:49:02 +0600

    alhamdulillah done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { createContext, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-
+import Admin from './Components/Admin/Admin';
+import Checkout from './Components/Checkout/Checkout';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import ManageBooks from './Components/ManageBooks/ManageBooks';
+import Nav from './Components/Nav/Nav';
+import NotFound from './Components/NotFound/NotFound';
+import Orders from './Components/Orders/Orders';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+export const UserContext = createContext();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loggedInUser, setLoggedInUser] = useState({});
+    const [user, setUser] = useState({});
+    return (
+        <div className='App h-full '>
+            <UserContext.Provider
+                value={[loggedInUser, setLoggedInUser, user, setUser]}
+            >
+                <Router>
+                    <div>
+                        <Nav></Nav>
+
+                        {/* <hr /> */}
+
+                        <Switch>
+                            <Route exact path='/'>
+                                <Home />
+                            </Route>
+                            <PrivateRoute path='/admin'>
+                                <Admin />
+                            </PrivateRoute>
+                            <PrivateRoute path='/order'>
+                                <Orders />
+                            </PrivateRoute>
+                            <Route path='/login'>
+                                <Login></Login>
+                            </Route>
+                            <PrivateRoute path='/book/:bookId'>
+                                <Checkout></Checkout>
+                            </PrivateRoute>
+                            <Route path='*'>
+                                <NotFound></NotFound>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </UserContext.Provider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyCUIatsXIMrVHGBooJABgWQcK38vzsTPK8',
+    authDomain: 'book-shelf-4c0d0.firebaseapp.com',
+    projectId: 'book-shelf-4c0d0',
+    storageBucket: 'book-shelf-4c0d0.appspot.com',
+    messagingSenderId: '364028137866',
+    appId: '1:364028137866:web:20943bb4dd5c9f0a67b873',
+    measurementId: 'G-R08CLZ0XR0',
+};
+export default firebaseConfig;
