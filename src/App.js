3d8cb5754aa47193a2ae0459867776b62d6b0a0a
Arshik Hossen Akash <arshikakash@gmail.com>
2022-12-26 20:33:07 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { createContext, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -16,17 +14,7 @@ export const UserContext = createContext();
 function App() {
     const [loggedInUser, setLoggedInUser] = useState({});
     const [user, setUser] = useState({});
-    const firebaseConfig = {
-        apiKey: 'AIzaSyCUIatsXIMrVHGBooJABgWQcK38vzsTPK8',
-        authDomain: 'book-shelf-4c0d0.firebaseapp.com',
-        projectId: 'book-shelf-4c0d0',
-        storageBucket: 'book-shelf-4c0d0.appspot.com',
-        messagingSenderId: '364028137866',
-        appId: '1:364028137866:web:20943bb4dd5c9f0a67b873',
-        measurementId: 'G-R08CLZ0XR0',
-    };
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+
     return (
         <div className='App h-full '>
             <UserContext.Provider
